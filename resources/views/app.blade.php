4d993689e8f012b4c0ba45afa1fea40c9a77628d
Carlos Moneda <moneda.psc@gmail.com>
2016-11-04 10:25:04 -0600

    schema full

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -67,14 +67,22 @@
   {
     "@context": "http://schema.org",
     "@type": "Organization",
-    "name": "PHP Mexico",
-    "alternateName": "PHP Mexico the Right Way",
-    "url": "https://chat.phpmexico.mx/",
+    "url": "https://phpmexico.mx",
+    "logo": "https://phpmexico.mx/sites/default/files/logo_huichol.png",
+    "description": "La comunidad de PHP más avanzada de México. Nosotros tomamos en serio PHP y queremos regresar la dignidad al lenguaje, nos reunimos mensualmente en la ciudad de México a compartir conocimiento.",
+    "sponsor": {
+      "@type": "Organization",
+      "name": "Indava",
+      "url": "https://indava.com/"
+    },
+    "disambiguatingDescription": "Comunidad de programadores PHP en México",
     "sameAs": [
       "https://www.facebook.com/pehacheperos",
       "https://twitter.com/phpmx",
-      "https://www.meetup.com/PHP-The-Right-Way/",
-      "https://chat.phpmexico.mx/"
+      "https://www.meetup.com/es/PHP-The-Right-Way/",
+      "https://phpmx.slack.com/",
+      "https://github.com/phpwaymx",
+      "https://chat.phpmexico.mx"
     ]
   }
   </script>
